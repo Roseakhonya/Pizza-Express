957d4ff84f747ea5534c8c0de4e5f32a4a8baab4
Roseakhonya <akhonyarose@gmail.com>
2019-11-11 09:55:29 +0300

    Declare variables and linking the js files

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var order = function(size, crust, toppings){
+    this.size=size;
+    this.crust=crust;
+    this.toppings=toppings;
+    this.totalCost= cost;  
+}
+var size = ["Small","Medium", "Large"];
+var crust = [""]
