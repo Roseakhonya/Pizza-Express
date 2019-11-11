c1321ff4e7a09ea831b79baa8a58048397dbbed4
Roseakhonya <akhonyarose@gmail.com>
2019-11-11 16:56:57 +0300

    add calculation

--- js/test.js
+++ js/test.js
@@ -1,20 +1,29 @@
-$(document).ready(
-    function(){
-        $('#checkout').click(function(){
-            var size = parseInt($("#size option:selected").val());
-            var crust = parseInt($("#crust option:selected").val());
-            var topping = parseInt($("#topping option:selected").val());
-            var quantity = parseInt($("#quantity").val());
-            var name = $("#fullname").val();
-            var phone = $("#phone").val();
-            var pick  = $("input[name='pick']:checked"). val()
-        
-            var total = quantity * (size + crust + topping);
+$(document).ready(function() {
+  $("#checkout").click(function() {
+    var size = parseInt($("#size option:selected").val());
+    var crust = parseInt($("#crust option:selected").val());
+    var topping = parseInt($("#topping option:selected").val());
+    var quantity = parseInt($("#quantity").val());
+    var name = $("#fullname").val();
+    var phone = $("#phone").val();
+    var pick = $("input[name='pick']:checked").val();
+    var deliveryCost = parseInt($("#cost").val());
 
-            alert("Hello "   +   name  +  " your  order   costs "    +    total);
-            //alert("Hello"  +  name  +  "your order costs" + total);
-        }
-            
-        )
+    var total = quantity * (size + crust + topping);
+      alert("Hello " + name + " your  order   costs " + total +
+      " and your delivery cost is " +
+      deliveryCost);
+      //alert("Hello"  +  name  +  "your order costs" + total);
+      if (pick === 2) {
+        var totalCost = quantity * (size + crust + topping);
+        alert(
+          "Hello " +
+            name +
+            " your  order   costs " +
+            totalCost +
+            " and your delivery cost is " +
+            deliveryCost
+        );
     }
-)
+  });
+});
